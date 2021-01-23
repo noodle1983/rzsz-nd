c5d4c2518cd8e9a1d354dd9d1d0ad2165c06bd9d
LuDong <noodle1983@126.com>
2021-01-23 10:40:08 +0800

    single thread

--- io/stdinput.h
+++ io/stdinput.h
@@ -15,61 +15,75 @@ namespace nd
     {
     public:
         StdInput()
-            : processorM(1)
-            , bufferM(20) // 1MB
-            , stopM(false)
+            //: processorM(1)
+            //, bufferM(20) // 1MB
+            //, stopM(false)
         {
+            setTtyRawMode(0);
             initZmodemTab();
             setvbuf(stdin, NULL, _IONBF, 0);
             fcntl(0, F_SETFL, fcntl(0, F_GETFL) | O_NONBLOCK);
-            processorM.start();
-            processorM.process(1, NEW_JOB(std::bind(&StdInput::read, this)));
+            //processorM.start();
+            //processorM.process(1, NEW_JOB(std::bind(&StdInput::read, this)));
         }
 
-        static void alarmHandler(int dummy){}
 
         virtual ~StdInput(){
-            stopM = true;
-            signal(SIGALRM, alarmHandler);
-            alarm(1);
-            processorM.stop();
-        }
-
-        void read() {
-            fd_set fds;
-            struct timeval timeout;
-            timeout.tv_sec = 0;
-            timeout.tv_usec = 10*1000;
-            while(!stopM && bufferM.isHealthy()){
-                FD_SET(0, &fds);
-                if(select(1, &fds, NULL, NULL, &timeout)<0){
-                    LOG_ERROR("select failed! errno:" << errno);
-                    return;
-                }
-                if (!FD_ISSET(0, &fds)){continue;}
-
-                char buf[1024];
-                int len = ::read(0, buf, sizeof(buf));
-                if (len > 0){
-                    bufferM.putn(buf, len);
-                }
-            }
+            //stopM = true;
+            //processorM.stop();
         }
 
         int getInput(char* buf, unsigned len){
-            bool stopped = !bufferM.isHealthy();
-            
-            int readed = bufferM.get(buf, len);
-            if (stopped && bufferM.isHealthy()){
-                processorM.process(1, NEW_JOB(std::bind(&StdInput::read, this)));
+            fd_set fds;
+            struct timeval timeout;
+            timeout.tv_sec = 0;
+            timeout.tv_usec = 0;//10*1000;
+            FD_SET(0, &fds);
+            if(select(1, &fds, NULL, NULL, &timeout)<0){
+                LOG_ERROR("select failed! errno:" << errno);
+                return 0;
             }
+            if (!FD_ISSET(0, &fds)){return 0;}
+
+            int readed = ::read(0, buf, len);
             return readed;
         }
 
+        //void read() {
+        //    fd_set fds;
+        //    struct timeval timeout;
+        //    timeout.tv_sec = 0;
+        //    timeout.tv_usec = 0;//10*1000;
+        //    while(!stopM && bufferM.isHealthy()){
+        //        FD_SET(0, &fds);
+        //        if(select(1, &fds, NULL, NULL, &timeout)<0){
+        //            LOG_ERROR("select failed! errno:" << errno);
+        //            return;
+        //        }
+        //        if (!FD_ISSET(0, &fds)){continue;}
+
+        //        char buf[1024];
+        //        int len = ::read(0, buf, sizeof(buf));
+        //        if (len > 0){
+        //            bufferM.putn(buf, len);
+        //        }
+        //    }
+        //}
+
+        //int getInput(char* buf, unsigned len){
+        //    bool stopped = !bufferM.isHealthy();
+        //    
+        //    int readed = bufferM.get(buf, len);
+        //    if (stopped && bufferM.isHealthy()){
+        //        processorM.process(1, NEW_JOB(std::bind(&StdInput::read, this)));
+        //    }
+        //    return readed;
+        //}
+
     private:
-        CppProcessor processorM;
-        nd::KfifoBuffer bufferM;
-        std::atomic<bool> stopM;
+        //CppProcessor processorM;
+        //nd::KfifoBuffer bufferM;
+        //std::atomic<bool> stopM;
     };
 
 }
