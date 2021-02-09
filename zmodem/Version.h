02c8a22666070d6563679f982ab988c5da5b7f02
LuDong <noodle1983@126.com>
2021-02-09 11:59:26 +0800

    update version changes

--- zmodem/Version.h
+++ zmodem/Version.h
@@ -12,6 +12,8 @@
  * 1. sz/rz dir.
  *      a. support relative path.
  * 2. support large file(>4GB)
+ * 3. check existing file's CRC and then resume/skip/retransmit if previous transfer is broke/completed/invalid.
+ * 4. options to update client/server working direction
  */
 const uint8_t ZVERSION = 1;
 
