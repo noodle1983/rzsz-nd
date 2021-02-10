498770680c83fc36e28802766811cbf92a9cfb9d
LuDong <noodle1983@126.com>
2021-02-10 11:41:20 +0800

    add lic

--- fsm/Fsm.cpp
+++ fsm/Fsm.cpp
@@ -1,3 +1,10 @@
+/**
+ * Licensing Information
+ *    This is a release of rzsz-nd, brought to you by Dong Lu(noodle1983@126
+ *    .com). Except for extra permissions from Dong Lu(noodle1983@126.com),
+ *    this software is released under version 3 of the GNU General
+ *    Public License (GPLv3).
+ **/
 #include "Fsm.h"
 
 using namespace nd;
