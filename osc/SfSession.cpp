6966a440da7876993c390e618f1a479c1c17ac27
LuDong <noodle1983@126.com>
2023-09-03 22:18:13 +0800

    enlarge bye timeout

--- osc/SfSession.cpp
+++ osc/SfSession.cpp
@@ -31,7 +31,7 @@ nd::FiniteStateMachine* SfSession::getZmodemFsm()
         (*fsm) +=      FSM_EVENT(ENTRY_EVT,      SE_FUNC(OscSession, sendInitReq));
         (*fsm) +=      FSM_EVENT(ENTRY_EVT,      NEW_TIMER(1500));
         (*fsm) +=      FSM_EVENT(TIMEOUT_EVT,    SE_FUNC(OscSession, unsupportNotice));
-        (*fsm) +=      FSM_EVENT(TIMEOUT_EVT,    CHANGE_STATE(END_STATE));
+        (*fsm) +=      FSM_EVENT(TIMEOUT_EVT,    CHANGE_STATE(DESTROY_STATE));
         (*fsm) +=      FSM_EVENT(RESET_EVT,      CHANGE_STATE(END_STATE));
         (*fsm) +=      FSM_EVENT(NETWORK_INPUT_EVT, SE_FUNC(OscSession, parsePkg));
         (*fsm) +=      FSM_EVENT(NEXT_EVT,       SE_FUNC(OscSession, sendEmptyDirs));
@@ -64,7 +64,7 @@ nd::FiniteStateMachine* SfSession::getZmodemFsm()
 
         (*fsm) += FSM_STATE(END_STATE);	
         (*fsm) +=      FSM_EVENT(ENTRY_EVT,         SE_FUNC(OscSession, sendBye));
-        (*fsm) +=      FSM_EVENT(ENTRY_EVT,         NEW_TIMER(1000));
+        (*fsm) +=      FSM_EVENT(ENTRY_EVT,         NEW_TIMER(3000));
         (*fsm) +=      FSM_EVENT(TIMEOUT_EVT,       CHANGE_STATE(DESTROY_STATE));
         (*fsm) +=      FSM_EVENT(NETWORK_INPUT_EVT, SE_FUNC(OscSession, parsePkg));
         (*fsm) +=      FSM_EVENT(DESTROY_EVT,       CHANGE_STATE(DESTROY_STATE));
